b651881cdba9f5213fa888b3d1c7d0968d110b82
Eddie Gyger <eddiegyger92@gmail.com>
2025-10-12 11:52:48 -0500

    Add Porcupine Package

---
+++ UD1Chassis/Options/PorcupineOptions.cs
@@ -0,0 +1,6 @@
+namespace UD1Chassis.Options;
+
+public class PorcupineOptions
+{
+    public required string ApiKey { get; set; }
+}
--- UD1Chassis/Program.cs
+++ UD1Chassis/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.Extensions.Options;
 using UD1Chassis;
+using UD1Chassis.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.AddOptions<PorcupineOptions>()
+    .Bind(builder.Configuration.GetSection(nameof(PorcupineOptions)))
+    .ValidateOnStart();
 
 var host = builder.Build();
 host.Run();
